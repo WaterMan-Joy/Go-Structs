cc412a3563216a4902e44aab41e261878074fca2
WaterMan-Joy <nietzsche21st@gmail.com>
2023-08-27 11:20:34 +0900

    basic and seconde struct

--- main.go
+++ main.go
@@ -5,12 +5,23 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
 }
 
 func main() {
-	var joy person
-	joy.firstName = "Joy"
-	joy.lastName = "Kim"
+	joy := person{
+		firstName: "Joy",
+		lastName:  "Kim",
+		contact: contactInfo{
+			email:   "<EMAIL>",
+			zipCode: 12345,
+		},
+	}
 
 	fmt.Printf("%+v", joy)
 }
