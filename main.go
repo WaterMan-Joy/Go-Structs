f515929037ea33111fe233d44b623cc58a2aef04
WaterMan-Joy <nietzsche21st@gmail.com>
2023-08-27 16:49:37 +0900

    editting

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
 }
 
 type contactInfo struct {
@@ -15,13 +15,31 @@ type contactInfo struct {
 
 func main() {
 	joy := person{
-		firstName: "Joy",
+		firstName: "joy",
 		lastName:  "Kim",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "<EMAIL>",
 			zipCode: 12345,
 		},
 	}
+	// joy.updateNameCopy("Han", "Y")
+	// fmt.Println(&joy.firstName)
+	// joyPointer := &joy
+	joy.updateName("Sunny", "J")
+	joy.print()
+	fmt.Println(&joy.firstName)
+}
 
-	fmt.Printf("%+v", joy)
+func (p person) updateNameCopy(firstName, lastName string) {
+	p.firstName = firstName
+	p.lastName = lastName
+}
+
+func (pointerToPoerson *person) updateName(firstName string, lastName string) {
+	(*pointerToPoerson).firstName = firstName
+	(*pointerToPoerson).lastName = lastName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
